75696aeb5e9d390b7af3d1ba6a41cedb77cc86f3
mohandassdt <vijimohandass.s@gmail.com>
2017-07-27 06:04:07 -0700

    Update appointmentController.js

--- client/controller/appointmentController.js
+++ client/controller/appointmentController.js
@@ -21,32 +21,32 @@ angular.module('tatluApp').controller('appointmentController', function($scope,$
       }
     }];
     vm.events = [
-      {
-        title: 'An event',
-        color: calendarConfig.colorTypes.warning,
-        startsAt: moment().startOf('week').subtract(2, 'days').add(8, 'hours').toDate(),
-        endsAt: moment().startOf('week').add(1, 'week').add(9, 'hours').toDate(),
-        draggable: true,
-        resizable: true,
-        actions: actions
-      }, {
-        title: '<i class="glyphicon glyphicon-asterisk"></i> <span class="text-primary">Another event</span>, with a <i>html</i> title',
-        color: calendarConfig.colorTypes.info,
-        startsAt: moment().subtract(1, 'day').toDate(),
-        endsAt: moment().add(5, 'days').toDate(),
-        draggable: true,
-        resizable: true,
-        actions: actions
-      }, {
-        title: 'This is a really long event title that occurs on every year',
-        color: calendarConfig.colorTypes.important,
-        startsAt: moment().startOf('day').add(7, 'hours').toDate(),
-        endsAt: moment().startOf('day').add(19, 'hours').toDate(),
-        recursOn: 'year',
-        draggable: true,
-        resizable: true,
-        actions: actions
-      }
+//       {
+//         title: 'An event',
+//         color: calendarConfig.colorTypes.warning,
+//         startsAt: moment().startOf('week').subtract(2, 'days').add(8, 'hours').toDate(),
+//         endsAt: moment().startOf('week').add(1, 'week').add(9, 'hours').toDate(),
+//         draggable: true,
+//         resizable: true,
+//         actions: actions
+//       }, {
+//         title: '<i class="glyphicon glyphicon-asterisk"></i> <span class="text-primary">Another event</span>, with a <i>html</i> title',
+//         color: calendarConfig.colorTypes.info,
+//         startsAt: moment().subtract(1, 'day').toDate(),
+//         endsAt: moment().add(5, 'days').toDate(),
+//         draggable: true,
+//         resizable: true,
+//         actions: actions
+//       }, {
+//         title: 'This is a really long event title that occurs on every year',
+//         color: calendarConfig.colorTypes.important,
+//         startsAt: moment().startOf('day').add(7, 'hours').toDate(),
+//         endsAt: moment().startOf('day').add(19, 'hours').toDate(),
+//         recursOn: 'year',
+//         draggable: true,
+//         resizable: true,
+//         actions: actions
+//       }
     ];
 
     vm.cellIsOpen = true;
