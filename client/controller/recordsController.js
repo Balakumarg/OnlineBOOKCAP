e3d74ccbbea3bc1673a2647d0198c3ff058cb630
mohandassdt <vijimohandass.s@gmail.com>
2017-08-04 06:25:51 +0530

    Update recordsController.js

--- client/controller/recordsController.js
+++ client/controller/recordsController.js
@@ -1,12 +1,148 @@
+// angular.module('tatluApp').controller('recordsController', function($scope, $http,$cookies,AuthenticationService,$location,fileUpload) {
+//
+//
+//   var authUser = $cookies.getObject('authUser');
+//   var Userfname = authUser.currentUser.userInfo.fname;
+//   var UserID = authUser.currentUser.userInfo.Id;
+//
+//   $scope.fname = Userfname;
+//   $scope.Id = UserID;
+//
+//
+//
+//
+//
+// //
+// //
+// //
+// //     //  $scope.arraylist=[];
+// //
+// //
+// //       //getting menubar details from database//
+// //         $scope.refreshrecords = function () {
+// //               $http.get('/records/records').success(function (response) {
+// //                   console.log(' READ IS SUCCESSFUL');
+// //                   $scope.recordsList = response;
+// //                     $scope.records = "";
+// //
+// //
+// //               });
+// //           };
+// //
+// //
+// //   refreshrecords();
+// //
+// //
+// //
+// //
+// //
+// //
+// //
+// // //
+// // //   var refreshrecords = function () {
+// // //         $http.get('/records/records').success(function (response) {
+// // //             $scope.recordslist = response;
+// // //
+// // //             for(var i=0;i<$scope.recordList.length;i++){
+// // //
+// // //
+// // //               if($scope.recordList[i].==UserID){
+// // //                 console.log($scope.recordList[i]);
+// // //   $scope.arraylist.push($scope.recordList[i]);
+// // //
+// // // console.log($scope.arraylist);
+// // //
+// // //               }
+// // //
+// // //
+// // //             }
+// // //
+// // //               $scope.records ="";
+// // //         });
+// // //     };
+// // //     refreshrecords();
+// // //
+// //
+// //
+// //
+// //
+// //
+// //
+// //
+// //
+//
+// // ************************************************************************************************//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//   $scope.addrecords=function(){
+//
+//                 var file =$scope.recordfile;
+//                 var uploadUrl = "/savedata1";
+//                 fileUpload.uploadFileToUrl(file, uploadUrl);
+//
+//           $scope.records.recordfilename=UserID+$scope.recordfile.name;
+//           $scope.records.patient_name= Userfname;
+//           $scope.records.patientid=UserID;
+//
+//           $http.post('/records/records', $scope.records).success(function (response) {
+//           console.log(response);
+//
+//   //$rootScope.petientDetails=response;
+//
+//           alert("File uploaded completed!!!!");
+//
+//           });
+//
+//
+//
+//
+//
+//   }
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 angular.module('tatluApp').controller('recordsController', function($scope, $http,$cookies,AuthenticationService,$location,fileUpload) {
 
 
-  var authUser = $cookies.getObject('authUser');
-  var Userfname = authUser.currentUser.userInfo.fname;
-  var UserID = authUser.currentUser.userInfo.Id;
-
-  $scope.fname = Userfname;
-  $scope.Id = UserID;
 
 
 
@@ -73,17 +209,38 @@ angular.module('tatluApp').controller('recordsController', function($scope, $htt
 
 // ************************************************************************************************//
 
+var refreshrecords = function () {
+
+  
+               $http.get('/records/records').success(function (response) {
+                   console.log(' READ IS SUCCESSFUL');
+                   $scope.recordsList = response;
+                     $scope.records = "";
 
 
 
 
 
+               });
+           };
 
+     refreshrecords();
 
 
 
 
   $scope.addrecords=function(){
+
+    var authUser = $cookies.getObject('authUser');
+    var Userfname = authUser.currentUser.userInfo.fname;
+    var UserID = authUser.currentUser.userInfo.Id;
+
+    $scope.fname = Userfname;
+    $scope.Id = UserID;
+
+
+
+
 
                 var file =$scope.recordfile;
                 var uploadUrl = "/savedata1";
